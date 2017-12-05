2d0f0c126c5d1d5b2c1b7fa82adffa546833ee28
Uilan Souza <uilan.souza@hotmail.com>
2017-12-04 22:00:47 -0200

    logado alterado

--- login/logado.php
+++ login/logado.php
@@ -3,6 +3,16 @@
 session_name('TREINAWEB');
 session_start();
 
+if (!isset($_SESSION['sessIdTime'])) {
+        $_SESSION['sessIdTime'] = time(); // Cria a sessão caso nao exista
+    } elseif (time() - $_SESSION['sessIdTime']>10) {// Caso a diferença da sessão criada para o horário atual for maior que 10 segundos então a sessão é derrubada
+        $_SESSION = [];
+        session_destroy(); //destroi a sessão
+    }
+    else{
+    $_SESSION['sessIdTime']=time();
+}
+
 if (!isset($_SESSION['logado'])) {
     header('Location: index.php');
     exit;
