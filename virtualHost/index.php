687bdc4e2dcd703e02d6007a001e672599627074
Uilan Souza <uilan.souza@hotmail.com>
2017-12-13 17:10:05 -0200

    ok

---
+++ virtualHost/index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Hello word!";
+
+/* Você deve fazer as alterações no arquivo host do windows no diretório
+C:\Windows\System32\drivers\etc
+ * Você ira criar um arquivo chamado MeuProjeto no diretório C:\MeuProjeto
+ * o WIndows irá criar um servidor para a URL treinaweb.dev assim como esta configurado 
+ * no arquivo de host
+
+ */
+
+
+
+?>
